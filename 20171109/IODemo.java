29efe0e59e4f10a2fa41d5bc5bc7702e6cfb7f44
jiangbo <bo.jiang@onebank.com.cn>
2017-11-13 14:00:08 +0800

    init old

---
+++ 20171109/IODemo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class IODemo {
+  public static void main(String[] args) {
+    String fileName = "D:" + File.separator;
+    File f = new File(fileName);
+    String[] str = f.list();
+    for (int i = 0; i < str.length; i++) {
+      System.out.println(str[i]);
+    }
+  }
+}
